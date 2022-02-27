507115206c243f36bbae53d6dd61fef052770e79
Zita <ambrus.zita99@gmail.com>
2022-02-27 17:14:33 +0100

    Kettesig

--- src/hu/petrik/zita/Fuvarok.java
+++ src/hu/petrik/zita/Fuvarok.java
@@ -27,6 +27,25 @@ public class Fuvarok {
         }
 
     }
+    public long utazasokSzama(){
+        return fuvarok.stream().count();
+    }
+
+    public double doll(double a){
+        return fuvarok.stream().filter(fuvar -> fuvar.getTaxi_id()== fuvar.getTaxi_id()).mapToDouble(fuvar -> fuvar.getViteldij()+fuvar.getBorravalo()).sum();
+    }
+    public long db(int a){
+        return fuvarok.stream().filter(fuvar -> fuvar.getTaxi_id()== fuvar.getTaxi_id()).count();
+    }
+    public String bevetelFuvar(int a){
+        double bevetel;
+        long szam;
+        szam=db(a);
+        bevetel=doll(a);
+
+        return String.format(a+" azonósító számmal rendelkező taxinak a bevétele %4.2f $, ami %d fuvarból állt.",bevetel,szam);
+    }
+
 
 
     @Override
--- src/hu/petrik/zita/Main.java
+++ src/hu/petrik/zita/Main.java
@@ -5,5 +5,30 @@ public class Main {
     public static void main(String[] args) {
         Fuvarok FU= new Fuvarok("fuvar.csv");
         System.out.println(FU);
+
+        System.out.println("1. Feladat");
+        System.out.println(FU.utazasokSzama()+" db");
+
+        System.out.println("\n2. Feladat");
+        System.out.println(FU.bevetelFuvar(6185));
+
+        System.out.println("\n3. Feladat");
+
+        System.out.println("\n4. Feladat");
+
+        System.out.println("\n5. Feladat");
+
+        System.out.println("\n6. Feladat");
+
+        System.out.println("\n7. Feladat");
+
+        System.out.println("\n8. Feladat");
+
+        System.out.println("\n9. Feladat");
+
+        System.out.println("\n10. Feladat");
+
+        System.out.println("\n11. Feladat");
     }
+
 }
