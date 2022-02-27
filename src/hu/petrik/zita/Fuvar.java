ed146217af1748009f269b3cac1b40f78a06e51b
Zita <ambrus.zita99@gmail.com>
2022-02-27 16:32:37 +0100

    Mert miért ne

--- src/hu/petrik/zita/Fuvar.java
+++ src/hu/petrik/zita/Fuvar.java
@@ -4,15 +4,14 @@ package hu.petrik.zita;
 import java.time.LocalDateTime;
 
 public class Fuvar {
-    private String fizetes_modja;
+    private String fizetes_modja,indulas;
     private int taxi_id,idotartam;
-    private LocalDateTime indulas;
     private Double tavolsag,viteldij,borravalo;
 
     public Fuvar(String s){
         String[] adatok=s.split(";");
         this.taxi_id=Integer.parseInt(adatok[0]);
-
+        this.indulas = adatok[1];
         this.idotartam=Integer.parseInt(adatok[2]);
         this.tavolsag= Double.parseDouble(adatok[3].replace(",", "."));
         this.viteldij= Double.parseDouble(adatok[4].replace(",", "."));
@@ -44,11 +43,11 @@ public class Fuvar {
         this.idotartam = idotartam;
     }
 
-    public LocalDateTime getIndulas() {
+    public String getIndulas() {
         return indulas;
     }
 
-    public void setIndulas(LocalDateTime indulas) {
+    public void setIndulas(String indulas) {
         this.indulas = indulas;
     }
 
@@ -78,14 +77,9 @@ public class Fuvar {
 
     @Override
     public String toString() {
-        return "Fuvar{" +
-                "fizetes_modja='" + fizetes_modja + '\'' +
-                ", taxi_id=" + taxi_id +
-                ", idotartam=" + idotartam +
-                ", indulas=" + indulas +
-                ", tavolsag=" + tavolsag +
-                ", viteldij=" + viteldij +
-                ", borravalo=" + borravalo +
-                '}';
+        return String.format("Taxi azonosítója: %-7d Indulás időpontja: %-22s Utazás időtartama: %5d mp " +
+                        "\nUtazás távolsága: %4.2f m  Utazás viteldíja: %4.2f $   Taxis borravalója: %5.2f $   Fizetés módja: %12s\n",
+                this.taxi_id,this.indulas,this.idotartam,this.tavolsag,this.viteldij,this.borravalo,this.fizetes_modja);
+
     }
 }
